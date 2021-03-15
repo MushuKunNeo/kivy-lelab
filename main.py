fd6f9b2cd3b3a2f954d5bb86fc89d09e105842bc
Manuel <manu.gregam@gmail.com>
2021-03-15 17:05:10 +0100

    Add Files for Canvas Views

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
+from canvas_exemples import *
 from navigation_screen_manager import NavigationScreenManager
 
 class MyScreenManager(NavigationScreenManager):
@@ -19,5 +20,6 @@ class LeLabApp(App):
     def build(self):
         self.manager = MyScreenManager()
         return  self.manager
+        #return CanvasExemple7()
 
 LeLabApp().run()
